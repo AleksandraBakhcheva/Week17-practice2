693be2d32ade1594ccd7c626d49eabcd88aafb9f
Aleksandra <alexandra.bakhcheva@gmail.com>
2022-07-27 21:36:52 +0500

    html tags moved to js, added empty message check

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+let container = document.createElement("div");
+container.classList.add("container");
+container.innerHTML += `<div class="result"></div>
+<label for="messages"><input class="messages" type="text" id="messages" placeholder="Введите сообщение" /></label>
+<button>Отправить</button>`;
+document.body.append(container);
+
 let button = document.querySelector("button");
 button.addEventListener("click", addMessage);
 
 function addMessage() {
     let message = document.querySelector(".messages").value;
-    checkSpam(message);
-    document.querySelector(".messages").value = " ";
     
+    if (message !== "") {
+        checkSpam(message);
+        document.querySelector(".messages").value = "";
+    }
+    else {
+        return 0;
+    }
     function checkSpam(str) {
         let newstr = str.replace(/viagra/gi, "***");
         let finalstr = newstr.replace(/XXX/gi, "***");
